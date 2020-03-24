0c01ff4342e301d89859e8348288e79abefdffd2
Tauriq_Barron <tauriqbarron@hotmail.co.nz>
2020-03-24 15:45:22 +1300

    Added css for Index.blade.php

--- resources/views/Index.blade.php
+++ resources/views/Index.blade.php
@@ -1,8 +1,38 @@
+<!DOCTYPE html>
 <html>
-    <head>
+<head>
+    <link rel="stylesheet" href="{{url('css/Index.css')}}" type="text/css"/>
+</head>
+<body>
+<div class="MainCon">
+    <div class="header">
+        <div class="headerCon">
+            <button class="login">Login</button>
+            <button class="login">Sign up</button>
+        </div>
+    </div>
+    <hr>
+    <div class="homeNav">
+        <div class="homeNavCon">
+            <ul class="navBar">
+                <li class="topNav">Security</li>
+                <li class="topNav">Property</li>
+                <li class="topNav">News</li>
+            </ul>
+        </div>
+    </div>
+    <hr>
+    <div class="footer">
+        <div class="footerCon">
+            <ul class="footerLinks">
+                <li>Contact Us</li>
+                <li>About Us</li>
+                <li>Terms and Conditions</li>
+            </ul>
+        </div>
+    </div>
+    <p>Test</p>
+</div>
 
-    </head>
-    <body>
-        <p><h1>Big PP</h1></p>
-    </body>
+</body>
 </html>
