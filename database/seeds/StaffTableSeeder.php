40f46345fc4eb6757c233cb0986ee4514d20e669
HereJay <40294296+HereJay@users.noreply.github.com>
2020-04-24 01:43:54 +1200

    Hash the password of staff account.
    not thing special

--- database/seeds/StaffTableSeeder.php
+++ database/seeds/StaffTableSeeder.php
@@ -16,7 +16,7 @@ class StaffTableSeeder extends Seeder
             'last_name' => 'Wick',
             'email' => 'JohnW@gmail.com',
             'phone_number' => '1234567891',
-            'password' => 'JW3020'
+            'password' => Hash::make('JW3020')
         ]);
         $staff->save();
 
@@ -25,7 +25,7 @@ class StaffTableSeeder extends Seeder
             'last_name' => 'Wayne',
             'email' => 'BruceW@gmail.com',
             'phone_number' => '1234567890',
-            'password' => 'BW0605'
+            'password' => Hash::make('BW0605')
         ]);
         $staff->save();
 
@@ -34,7 +34,7 @@ class StaffTableSeeder extends Seeder
             'last_name' => 'Li',
             'email' => 'BurceL@gmail.com',
             'phone_number' => '1234567811',
-            'password' => 'BL5689'
+            'password' => Hash::make('BL5689')
         ]);
         $staff->save();
 
@@ -43,7 +43,7 @@ class StaffTableSeeder extends Seeder
             'last_name' => 'Tyson',
             'email' => 'MikeT@gmail.com',
             'phone_number' => '1230567891',
-            'password' => 'MT1045'
+            'password' => Hash::make('MT1045')
         ]);
         $staff->save();
     }
