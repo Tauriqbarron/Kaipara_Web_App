7e32aa37fc17ac6ee3c47480bdecd46a71985bd9
Tauriq_Barron <tauriqbarron@hotmail.co.nz>
2020-04-30 15:33:27 +1200

    updated service provider jobs model

--- app/Service_Provider_Job.php
+++ app/Service_Provider_Job.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service_Provider_Job extends Model
 {
-    protected $fillable = ['service_provider_id', 'job_id'];
+    protected $fillable = ['service_provider_id'];
 }
