eba9a8338c72c997c0e416d398df96509296183e
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2020-06-09 15:41:29 +1200

    adding create scripts for client

---
+++ php_rest_kiapara/api/post/create_client.php
@@ -0,0 +1,37 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: applications/json');
+header('Access-Control-Allow-Methods: POST');
+header('Access-Control-Allow-Headers: Access-Control-Allow-Headers,Content-Type,
+Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+include_once '../../config/Database.php';
+include_once '../../models/Client.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$client = new Client($db);
+
+$data = json_decode(file_get_contents("php://input"));
+
+$client->first_name = $data->first_name;
+$client->last_name = $data->last_name;
+$client->email = $data->email;
+$client->phone_number = $data->phone_number;
+$client->password = $data->password;
+$client->street = $data->street;
+$client->suburb = $data->suburb;
+$client->city = $data->city;
+$client->postcode = $data->postcode;
+
+if($client->create()){
+    echo json_encode(
+        array('message'=>'Client Created Successfully')
+    );
+}else{
+    echo json_encode(
+        array('message'=>'Client creation Failed')
+    );
+}
+
