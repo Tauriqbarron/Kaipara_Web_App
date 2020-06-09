16a08f4ffe5f12e8cc47cf10684bbb9282715fb0
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2020-06-09 12:17:12 +1200

    adding single record get to client model

--- php_rest_kiapara/models/Client.php
+++ php_rest_kiapara/models/Client.php
@@ -41,5 +41,42 @@ class Client{
     $stmt->execute();
     return $stmt;
     }
+    public function getSingleClient(){
+        $query = 'SELECT 
+        id,
+        created_at,
+        updated_at,
+        first_name,
+        last_name,
+        email,
+        phone_number,
+        password,
+        street,
+        suburb,
+        city,
+        postcode
+    FROM
+    ' . $this->table .'
+    WHERE
+        id = ?';
+
+    $stmt = $this->conn->prepare($query);
+    $stmt->bindParam(1,$this->id);
+    $stmt->execute();
+
+    $row = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    $this->created_at = $row['created_at'];
+    $this->updated_at = $row['updated_at'];
+    $this->first_name = $row['first_name'];
+    $this->last_name = $row['last_name'];
+    $this->email = $row['email'];
+    $this->phone_number = $row['phone_number'];
+    $this->password = $row['password'];
+    $this->street = $row['street'];
+    $this->suburb = $row['suburb'];
+    $this->city = $row['city'];
+    $this->postcode = $row['postcode'];
+    }
 
 }
