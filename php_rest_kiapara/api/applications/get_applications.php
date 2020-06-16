a55bf26959d695f9295333ada175e2de2b47f443
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2020-06-16 13:09:39 +1200

    adding get applications script

---
+++ php_rest_kiapara/api/applications/get_applications.php
@@ -0,0 +1,45 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: applications/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Applications.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$applications = new Applications($db);
+$result = $applications->getApplications();
+
+$num = $result->rowCount();
+
+if($num > 0 ){
+    $applications_arr = array();
+    $applications_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $applications_item = array(
+            'id'=>$id,
+            'client_id'=>$client_id,
+            'title'=>$title,
+            'description'=>$description,
+            'status'=>$status,
+            'imagePath'=>$imagePath,
+            'price'=>$price,
+            'date'=>$date,
+            'start_time'=>$start_time,
+            'finish_time'=>$finish_time,
+            'street'=>$street,
+            'suburb'=>$suburb,
+            'city'=>$city,
+            'postcode'=>$postcode
+        );
+        array_push($applications_arr['data'],$applications_item);
+    }
+    echo json_encode($applications_arr);
+}else{
+    echo josn_encode(
+        array('message'=>'No Applications Found')
+    );
+}
