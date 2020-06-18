173c111fe56fa2a7c8f46b484923343eb0ddbd78
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2020-06-18 21:30:49 +1200

    adding first booking script for client bookings

---
+++ php_rest_kiapara/api/bookings/get_client_bookings.php
@@ -0,0 +1,50 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: applications/json');
+
+include_once '../../config/Database.php';
+include_once '../../models/Bookings.php';
+
+$database = new Database();
+$db = $database->connect();
+
+$bookings = new Bookings($db);
+
+if(isset($_GET['id'])){
+    $bookings->client_id = $_GET['id'];
+}else{
+    echo json_encode(
+        array('message'=>'id not set')
+    );
+}
+
+$result = $bookings->getClientBookings();
+
+$num = $result->rowCount();
+
+if($num > 0){
+    $bookings_arr = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+        $bookings_item = array(
+            'id'=>$id,
+            'client_id'=>$client_id,
+            'description'=>$description,
+            'status'=>$status,
+            'date'=>$date,
+            'start_time'=>$start_time,
+            'finish_time'=>$finish_time,
+            'street'=>$street,
+            'suburb'=>$suburb,
+            'city'=>$city,
+            'end_date'=>$end_date
+        );
+        array_push($bookings_arr,$bookings_item);
+    }
+    echo json_encode($bookings_arr);
+}else{
+    echo josn_encode(
+        array('message'=>'No Bookings Found')
+    );
+}
