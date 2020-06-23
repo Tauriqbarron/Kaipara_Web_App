781f2dc1304ce535f6ffab39ef94337fe7b2729f
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2020-06-23 14:40:00 +1200

    update

--- php_rest_kiapara/api/staff/login.php
+++ php_rest_kiapara/api/staff/login.php
@@ -29,7 +29,7 @@ if(isset($_GET['password'])){
 
 $staff->login();
 
-if (password_verify($passCheck,$client->password)){
+if (password_verify($passCheck,$staff->password)){
     echo json_encode(
    array('message'=>'Pass')
 );
